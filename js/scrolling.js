fd6b4c8ab9da5fccd829af5ed46180257ce7a4e4
Aaminah1 <132408023+Aaminah1@users.noreply.github.com>
2024-04-29 06:48:11 +0200

    js improvement

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,32 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toc = document.getElementById('table-of-contents');
-    const headings = document.querySelectorAll('h2, h3, h4');
-    const list = document.createElement('ul');
-
-    headings.forEach((heading, index) => {
-        if (!heading.id) {
-            heading.id = `heading-${index}`; // Ensures the heading has an ID
-        }
-
-        const listItem = document.createElement('li');
-        const link = document.createElement('a');
-        link.href = `#${heading.id}`;
-        link.textContent = heading.textContent.trim();
-        listItem.appendChild(link);
-        list.appendChild(listItem);
-    });
-
-    toc.appendChild(list);
+  setupTableOfContents();
+  setupBackToTopButton();
 });
-//back to top button function stuff
-document.addEventListener('DOMContentLoaded', function() {
-    var backToTopButtons = document.querySelectorAll('.back-to-top');
-    
-    backToTopButtons.forEach(function(button) {
-      button.addEventListener('click', function(e) {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      });
-    });
+
+function setupTableOfContents() {
+  const toc = document.getElementById('table-of-contents');
+  const headings = document.querySelectorAll('h2, h3, h4');
+  const list = document.createElement('ul');
+
+  headings.forEach((heading, index) => {
+      ensureHeadingHasID(heading, index);
+      const listItem = createTOCItem(heading);
+      list.appendChild(listItem);
   });
-  
+
+  toc.appendChild(list);
+}
+
+function ensureHeadingHasID(heading, index) {
+  if (!heading.id) {
+      heading.id = `heading-${index}`; // Assign an ID if absent
+  }
+}
+
+function createTOCItem(heading) {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  link.href = `#${heading.id}`;
+  link.textContent = heading.textContent.trim();
+  listItem.appendChild(link);
+  return listItem;
+}
+
+function setupBackToTopButton() {
+  var backToTopButtons = document.querySelectorAll('.back-to-top');
+
+  backToTopButtons.forEach(function(button) {
+      button.addEventListener('click', function(e) {
+          e.preventDefault();
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+  });
+}
