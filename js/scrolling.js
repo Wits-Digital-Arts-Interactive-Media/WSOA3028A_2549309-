78610f244dbe1b0bf2101835e947620a90ff6b92
Aaminah1 <132408023+Aaminah1@users.noreply.github.com>
2024-05-21 00:24:07 +0200

    css

--- js/scrolling.js
+++ js/scrolling.js
@@ -42,4 +42,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
   }
-  
+ // JavaScript to handle the ToC highlight and scrolling
+ document.addEventListener('DOMContentLoaded', function () {
+  const tocLinks = document.querySelectorAll('.toc-container a');
+  const sections = document.querySelectorAll('main section');
+
+  function activateLink() {
+      let index = sections.length;
+
+      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+      tocLinks.forEach((link) => link.classList.remove('active'));
+      tocLinks[index].classList.add('active');
+  }
+
+  activateLink();
+  window.addEventListener('scroll', activateLink);
+});
